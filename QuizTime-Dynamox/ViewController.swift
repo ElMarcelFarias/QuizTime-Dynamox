14858a83f7052cba6548233563352e9fa971a899
ElMarcelFarias <marcel.leitefarias@gmail.com>
2024-02-18 12:19:58 -0300

    first commit!

---
+++ QuizTime-Dynamox/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QuizTime-Dynamox
+//
+//  Created by Marcel Leite de Farias on 18/02/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
